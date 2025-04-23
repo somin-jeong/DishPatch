fded5e6ee4f3e5ef68aab8d9fe10fa6c161a2a18
정소민 <yjk70702002@gmail.com>
2025-04-23 16:24:34 +0900

    feat: 메뉴 생성 api

---
+++ src/main/java/com/example/dishpatch/domain/store/service/StoreService.java
@@ -0,0 +1,54 @@
+package com.example.dishpatch.domain.store.service;
+
+import static com.example.dishpatch.domain.store.exception.StoreErrorCode.*;
+
+import org.springframework.stereotype.Service;
+
+import com.example.dishpatch.api.store.request.StoreCreateRequest;
+import com.example.dishpatch.api.store.response.StoreCreateResponse;
+import com.example.dishpatch.global.exception.BizException;
+import com.example.dishpatch.infra.db.store.entity.Category;
+import com.example.dishpatch.infra.db.store.entity.Store;
+import com.example.dishpatch.infra.db.store.repository.CategoryRepository;
+import com.example.dishpatch.infra.db.store.repository.StoreRepository;
+import com.example.dishpatch.infra.db.user.entity.User;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class StoreService {
+	private final StoreRepository storeRepository;
+	private final CategoryRepository categoryRepository;
+
+	public StoreCreateResponse createStore(User user, StoreCreateRequest request) {
+		// TODO: 사용자 role이 사장인지 확인
+
+		Category category = categoryRepository.findById(request.categoryId())
+			.orElseThrow(() -> new BizException(CATEGORY_NOT_FOUND));
+
+		int storeCount = storeRepository.countByUser(user);
+		if (storeCount >= 3) {
+			throw new BizException(STORE_OWN_LIMIT_EXCEEDED);
+		}
+
+		Store store = Store.builder()
+			.name(request.name())
+			.address(request.address())
+			.phone(request.phone())
+			.image(request.imageUrl())
+			.introduction(request.introduction())
+			.deliveryFee(request.deliveryFee())
+			.minDeliveryPrice(request.minDeliveryPrice())
+			.isAdvertised(request.isAdvertised())
+			.openTime(request.getOpenTime())
+			.closeTime(request.getCloseTime())
+			.user(user)
+			.category(category)
+			.build();
+
+		storeRepository.save(store);
+
+		return StoreCreateResponse.from(store);
+	}
+}
--- src/main/java/com/example/dishpatch/infra/db/store/repository/StoreRepository.java
+++ src/main/java/com/example/dishpatch/infra/db/store/repository/StoreRepository.java
@@ -3,6 +3,8 @@ package com.example.dishpatch.infra.db.store.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.dishpatch.infra.db.store.entity.Store;
+import com.example.dishpatch.infra.db.user.entity.User;
 
-public interface StoreRepository extends JpaRepository<Store, Integer> {
+public interface StoreRepository extends JpaRepository<Store, Long> {
+	int countByUser(User user);
 }

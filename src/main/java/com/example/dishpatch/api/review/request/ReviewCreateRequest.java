7dbf4322a55f6b117e6d049ca540a760a6a8b389
exmrim <exmrim@naver.com>
2025-04-23 14:44:04 +0900

    feat: 별점 범위 추가

--- src/main/java/com/example/dishpatch/api/review/request/ReviewCreateRequest.java
+++ src/main/java/com/example/dishpatch/api/review/request/ReviewCreateRequest.java
@@ -3,6 +3,7 @@ package com.example.dishpatch.api.review.request;
 import com.example.dishpatch.infra.db.review.entity.ReviewStatus;
 
 import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
@@ -12,6 +13,8 @@ public class ReviewCreateRequest {
 	private Long menuId;
 
 	@NotNull
+	@Min(1)
+	@Max(5)
 	private int rating;
 
 	@Max(255)

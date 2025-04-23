7f96577f68104000758c2e8af58991eff397da70
dungbik <q1w2e30630@gmail.com>
2025-04-23 12:09:35 +0900

    test: 메뉴 등록 기능 테스트 코드 작성

--- src/main/java/com/example/dishpatch/infra/db/store/repository/StoreRepository.java
+++ src/main/java/com/example/dishpatch/infra/db/store/repository/StoreRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.dishpatch.infra.db.store.entity.Store;
 
-public interface StoreRepository extends JpaRepository<Store, Integer> {
+public interface StoreRepository extends JpaRepository<Store, Long> {
 }

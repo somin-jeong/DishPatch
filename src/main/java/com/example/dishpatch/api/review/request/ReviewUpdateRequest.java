8eb13442d3fda86b45b4fb4c39a0fc37c220cd58
exmrim <exmrim@naver.com>
2025-04-23 20:02:33 +0900

    feat: 리뷰 수정 기능 구현

---
+++ src/main/java/com/example/dishpatch/api/review/request/ReviewUpdateRequest.java
@@ -0,0 +1,26 @@
+package com.example.dishpatch.api.review.request;
+
+import com.example.dishpatch.infra.db.review.entity.ReviewStatus;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+public record ReviewUpdateRequest(
+
+	@NotNull
+	@Min(1)
+	@Max(5)
+	Integer rating,
+
+	@Max(255)
+	String contents,
+
+	@Max(255)
+	String imageUrl,
+
+	@NotNull
+	ReviewStatus status
+
+) {
+}

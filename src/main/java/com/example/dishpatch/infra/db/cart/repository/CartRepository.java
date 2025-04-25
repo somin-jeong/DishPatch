a34e2dcd652572bf52a5d4f5bef7603232fa18da
exmrim <exmrim@naver.com>
2025-04-25 17:30:09 +0900

    feat: 장바구니 초기화 기능 구현

--- src/main/java/com/example/dishpatch/infra/db/cart/repository/CartRepository.java
+++ src/main/java/com/example/dishpatch/infra/db/cart/repository/CartRepository.java
@@ -1,5 +1,6 @@
 package com.example.dishpatch.infra.db.cart.repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -16,13 +17,16 @@ public interface CartRepository extends JpaRepository<Cart, Long> {
 
 	List<Cart> findByUserId(Long userId);
 
-
 	@Modifying(clearAutomatically = true, flushAutomatically = true)
 	@Query("UPDATE Cart c SET c.deletedDate = current_timestamp WHERE c.user.id = :userId")
-	//@Query("DELETE FROM Cart c WHERE c.user.id = :userId")
+		//@Query("DELETE FROM Cart c WHERE c.user.id = :userId")
 	void deleteAllByUserId(@Param("userId") Long userId);
 
 	@Modifying(clearAutomatically = true)
 	void deleteAllById(Long cartId);
+	
+	@Modifying
+	@Query("DELETE FROM Cart c WHERE c.modifiedDate <= :expiredTime")
+	void deleteAllExpired(@Param("expiredTime") LocalDateTime expiredTime);
 
 }

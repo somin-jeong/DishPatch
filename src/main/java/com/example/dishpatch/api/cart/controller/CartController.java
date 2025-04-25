3fb640aeaef1d48b200eb1a3aa72ac21262da598
exmrim <exmrim@naver.com>
2025-04-25 10:04:10 +0900

    feat: 장바구니 조회 기능 구현

--- src/main/java/com/example/dishpatch/api/cart/controller/CartController.java
+++ src/main/java/com/example/dishpatch/api/cart/controller/CartController.java
@@ -3,13 +3,16 @@ package com.example.dishpatch.api.cart.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.dishpatch.api.cart.request.CartCreateRequest;
 import com.example.dishpatch.api.cart.response.CartCreateResponse;
+import com.example.dishpatch.api.cart.response.CartResponseDto;
 import com.example.dishpatch.domain.cart.service.CartService;
 import com.example.dishpatch.global.security.UserAuth;
 
@@ -23,6 +26,7 @@ public class CartController {
 
 	private final CartService cartService;
 
+	@PostMapping("/cart")
 	public ResponseEntity<CartCreateResponse> createCart(
 		@PathVariable Long storeId,
 		@Valid @RequestBody CartCreateRequest request,
@@ -31,5 +35,15 @@ public class CartController {
 		CartCreateResponse response = cartService.createCart(storeId, request, userAuth);
 
 		return ResponseEntity.status(HttpStatus.CREATED).body(response);
+	}
+
+	@GetMapping("/carts")
+	public ResponseEntity<CartResponseDto> findCarts(
+		@PathVariable Long storeId,
+		@AuthenticationPrincipal UserAuth userAuth
+	) {
+		CartResponseDto response = cartService.findCarts(storeId, userAuth);
+
+		return ResponseEntity.status(HttpStatus.OK).body(response);
 	}
 }

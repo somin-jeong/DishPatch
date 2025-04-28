00cc3b7f12d499783adcb83a821db48946e8e13e
dungbik <q1w2e30630@gmail.com>
2025-04-28 09:18:57 +0900

    fix: path 별 권한 설정 잘못된 부분 수정

--- src/main/java/com/example/dishpatch/global/config/SecurityConfig.java
+++ src/main/java/com/example/dishpatch/global/config/SecurityConfig.java
@@ -40,11 +40,15 @@ public class SecurityConfig {
 			.authorizeHttpRequests(auth -> auth
 				.requestMatchers("/users/signup", "/users/login", "/stores", "/stores/{storeId}")
 				.permitAll()
+				.requestMatchers(HttpMethod.GET, "/stores/{storeId}/menus")
+				.permitAll()
 				.requestMatchers("/admin/**")
 				.hasRole("ADMIN")
-				.requestMatchers("/stores/{storeId}/menus", "/stores/{storeId}/menus/{menuId}",
+				.requestMatchers(HttpMethod.POST, "/stores/{storeId}/menus")
+				.hasRole("CEO")
+				.requestMatchers("/stores/{storeId}/menus/{menuId}",
 					"/stores/{storeId}/menus/{menuId}", "/menus/{menuId}/options", "/menus/{menuId}/options/{optionId}",
-					"/stores/{storeId}/stats/daily", "/stores/{storeId}/stats/monthly", "/orders/{orderId}/refuse",
+					"/stores/{storeId}/stats/orders", "/orders/{orderId}/refuse",
 					"/orders/{orderId}/update")
 				.hasRole("CEO")
 				.requestMatchers(HttpMethod.POST, "/ceoReviews/{reviewId}")
@@ -62,11 +66,11 @@ public class SecurityConfig {
 				.anyRequest()
 				.authenticated()
 			)
-			.oauth2Login(oauth ->
-				oauth.userInfoEndpoint(userInfo ->
-					userInfo.userService(customOAuth2UserService))
-					.successHandler(oAuth2SuccessHandler)
-			)
+			// .oauth2Login(oauth ->
+			// 	oauth.userInfoEndpoint(userInfo ->
+			// 		userInfo.userService(customOAuth2UserService))
+			// 		.successHandler(oAuth2SuccessHandler)
+			// )
 			.addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
 
 		return http.build();

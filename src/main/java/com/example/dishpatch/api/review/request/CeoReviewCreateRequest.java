b5a25e32720b46cee0dc3599e0d386285f6fab4b
exmrim <exmrim@naver.com>
2025-04-28 19:43:52 +0900

    feat: 어노테이션 수정 (Max -> Size)

--- src/main/java/com/example/dishpatch/api/review/request/CeoReviewCreateRequest.java
+++ src/main/java/com/example/dishpatch/api/review/request/CeoReviewCreateRequest.java
@@ -2,12 +2,12 @@ package com.example.dishpatch.api.review.request;
 
 import com.example.dishpatch.infra.db.review.entity.ReviewStatus;
 
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record CeoReviewCreateRequest(
 
-	@Max(255)
+	@Size(max = 255)
 	String contents,
 
 	@NotNull
